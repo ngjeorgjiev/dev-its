4373f8aa8862aaa55a4c5ed0490cdafce7b1bb41
Gjeorgijev Nikola <Nikola.Gjeorgijev@sanitas.com>
2020-12-01 00:54:47 +0100

    fix: updated packages

--- src/app/features/insert-card/insert-card.component.ts
+++ src/app/features/insert-card/insert-card.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 @Component({
   templateUrl: './insert-card.component.html',
   styleUrls: ['./insert-card.component.css']
 })
 export class InsertCardComponent {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private location: Location) { }
+  
+  goBack(): void {
+    this.location.back();
+  }
 
   invalid(): void {
     this.router.navigateByUrl('/checkout/card/insert-card/invalid');
